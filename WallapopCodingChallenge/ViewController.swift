c267c9fd256048c431d81a8427e4db2aa16d2a93
nikoagge <nikoagge@gmail.com>
2022-05-25 19:27:34 +0300

    Initial Commit

---
+++ WallapopCodingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallapopCodingChallenge
+//
+//  Created by Nikos Aggelidis on 25/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
