7f8023ba3f5f8a7d1c2ff3c71f5fe7302ec9e86b
nikoagge <nikoagge@gmail.com>
2022-05-26 18:14:50 +0300

    Initial reorganization of project structure based on MVVM design pattern.

---
+++ WallapopCodingChallenge/Resources/Credentials.swift
@@ -0,0 +1,11 @@
+//
+//  Credentials.swift
+//  WallapopCodingChallenge
+//
+//  Created by Nikos Aggelidis on 25/5/22.
+//
+
+import Foundation
+
+let publicKey = "3d1e58d37da19ae640e6d8333afd8b03"
+let privateKey = "9e251786605ab72bc1a3dd19ad16384adb7ba424"
