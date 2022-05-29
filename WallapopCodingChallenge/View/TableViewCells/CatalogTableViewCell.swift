3d9f940404e58a4f5e13bfdce7b21506e51a3156
nikoagge <nikoagge@gmail.com>
2022-05-29 09:51:37 +0300

    Made required init on CatalogTableViewCell unavailable for everyone

--- WallapopCodingChallenge/View/TableViewCells/CatalogTableViewCell.swift
+++ WallapopCodingChallenge/View/TableViewCells/CatalogTableViewCell.swift
@@ -33,6 +33,7 @@ class CatalogTableViewCell: UITableViewCell {
         setupUI()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
